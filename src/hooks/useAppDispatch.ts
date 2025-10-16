f5a33fd7f968ae254d018467987a3128492b6196
Mirjahon Ulmasov <mirjahonulmasov.com>
2025-10-16 10:32:59 +0500

    fixed store path

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -1,4 +1,4 @@
 import { useDispatch } from 'react-redux';
-import type { AppDispatch } from 'app/providers/StoreProvider';
+import type { AppDispatch } from 'store/store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -1,4 +1,4 @@
 import { type TypedUseSelectorHook, useSelector } from 'react-redux';
-import type { RootState } from 'app/providers/StoreProvider';
+import type { RootState } from 'store/store';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
